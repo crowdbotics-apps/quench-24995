2c173d61193d298b742033ae0e50e063f415007f
Crowdbotics <team@crowdbotics.com>
2021-03-12 11:30:52 +0000

    Update models

--- backend/quench_24995/wsgi.py
+++ backend/quench_24995/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quench_24995.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quench_24995.settings")
 
 application = get_wsgi_application()
